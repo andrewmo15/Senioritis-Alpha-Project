61b541086a25ac9bdd59e9cbbe43e002a188cd73
mayzhao2001 <67843945+mayzhao2001@users.noreply.github.com>
2023-03-14 03:48:07 -0400

    added color changing tile to room 1 in the main scene

---
+++ Assets/Scripts/FloorColorChange.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorColorChange : MonoBehaviour
+{
+
+    //public GameObject tile;
+    public Renderer tile;
+
+    public Material Red;
+    public Material Yellow;
+    public Material Blue;
+    public Material Green;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(changeColor());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator changeColor()
+    {
+        while (true)
+        {
+            tile.material = Red;
+            yield return new WaitForSeconds(1);
+            tile.material = Yellow;
+            yield return new WaitForSeconds(1);
+            tile.material = Blue;
+            yield return new WaitForSeconds(1);
+            tile.material = Green;
+            yield return new WaitForSeconds(1);
+        }
+        
+    }
+}
