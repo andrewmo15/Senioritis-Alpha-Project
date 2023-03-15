8224b0d0721c4bf694a3da3a9c9a0155dc18e2a8
Won Yang <yangwon700@gmail.com>
2023-03-14 21:44:21 -0400

    fix camera

---
+++ Assets/CameraTargetController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTargetController : MonoBehaviour
+{
+    public GameObject chicken;
+
+    private void LateUpdate()
+    {
+        // transform.position = chicken.transform.position + new Vector3(0, 0, 10);
+    }
+}
--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,20 +5,32 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject chicken;
-    private Vector3 positionOffset;
+    public GameObject cameraTarget;
+    private Vector3 cameraPositionOffset;
     private Quaternion rotationOffset;
+    private Transform chickenOffset;
+    private float xInit = 0f;
+    private float yInit = -1;
+    private float zInit = 2;
+    
+    public float damping = 1;
     
     // Start is called before the first frame update
     void Start()
     {
-        positionOffset = new Vector3(0,2, -5);
+        cameraPositionOffset = new Vector3(xInit, yInit, zInit);
         rotationOffset = Quaternion.Euler(0, 0, 0);
     }
 
     // Update is called once per frame
     void LateUpdate()
-    {
-        transform.position = chicken.transform.position + positionOffset;
-        // transform.rotation = chicken.transform.rotation * rotationOffset;
+    { 
+        float currentAngle = transform.eulerAngles.y;
+        float desiredAngle = chicken.transform.eulerAngles.y;
+        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, Time.deltaTime * damping);
+        Quaternion rotation = Quaternion.Euler(0, angle, 0); 
+        transform.position = chicken.transform.position - (rotation * cameraPositionOffset);
+        cameraTarget.transform.position = chicken.transform.position + (rotation * new Vector3(0, 0, 10));
+        transform.LookAt(cameraTarget.transform);
     }
 }
