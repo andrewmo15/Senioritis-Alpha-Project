12c878da4061b47f731d456cc417ead8338c8c9f
seonghun kang <sung@seonghuns-Air.attlocal.net>
2023-04-22 19:33:29 -0400

    Added instruction and plots

---
+++ Assets/Scripts/GameInstructionController.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameInstructionController : MonoBehaviour
+{
+    public GameObject plotPanel;
+    public GameObject instructionPanel;
+    public Button continueButton;
+    public float plotDisplayDuration = 5.0f; // Duration to display the plot in seconds
+
+    void Start()
+    {
+        // Set the button onClick listener
+        continueButton.onClick.AddListener(StartGame);
+
+        // Display the plot and instructions
+        StartCoroutine(DisplayPlotAndInstructions());
+    }
+
+    IEnumerator DisplayPlotAndInstructions()
+    {
+        // Pause the game
+        Time.timeScale = 0;
+
+        // Display the plot
+        plotPanel.SetActive(true);
+        instructionPanel.SetActive(false);
+
+        // Wait for the specified duration
+        yield return new WaitForSecondsRealtime(plotDisplayDuration);
+
+        // Hide the plot and display the instructions
+        plotPanel.SetActive(false);
+        instructionPanel.SetActive(true);
+    }
+
+    public void StartGame()
+    {
+        // Unpause the game
+        Time.timeScale = 1;
+
+        // Hide the instruction panel
+        instructionPanel.SetActive(false);
+    }
+}
